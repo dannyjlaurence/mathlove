cf7be39f79cfebed4150edb0f3882bee818dddd0
Daniel Laurence <dannyjlaurence@hotmail.com>
2013-12-15 11:11:11 -0500

    Please don't break!

--- src/com/neonbats/mathlove/LessonMap.java
+++ src/com/neonbats/mathlove/LessonMap.java
@@ -2,10 +2,17 @@ package com.neonbats.mathlove;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class LessonMap extends Activity {
 
 	public void onCreate(Bundle savedInstanceState) {
+		 super.onCreate(savedInstanceState);
+		 setContentView(R.layout.lessonmap);
+		 
+		 LinearLayout ll = new LinearLayout(this);
+		 ll.setBackgroundResource(R.drawable.path);
+		 
 	}
 	
 }
