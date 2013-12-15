1ea6cd93978d24f7f9c3ec81ccc765befc559bc6
Daniel Laurence <dannyjlaurence@hotmail.com>
2013-12-15 16:34:13 -0500

    Added first lesson page

--- gen/com/neonbats/mathlove/R.java
+++ gen/com/neonbats/mathlove/R.java
@@ -22,14 +22,18 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int path=0x7f020001;
-        public static final int shape=0x7f020002;
-        public static final int shape_droptarget=0x7f020003;
+        public static final int mandelbrotx1=0x7f020001;
+        public static final int mandelbrotx100=0x7f020002;
+        public static final int mandelbrotx2000=0x7f020003;
+        public static final int mandelbrotx6=0x7f020004;
+        public static final int path=0x7f020005;
+        public static final int shape=0x7f020006;
+        public static final int shape_droptarget=0x7f020007;
     }
     public static final class id {
-        public static final int action_settings=0x7f080019;
-        public static final int bottomleft=0x7f080015;
-        public static final int bottomright=0x7f080017;
+        public static final int action_settings=0x7f08001e;
+        public static final int bottomleft=0x7f08001a;
+        public static final int bottomright=0x7f08001c;
         public static final int button1=0x7f080003;
         public static final int button10=0x7f08000c;
         public static final int button2=0x7f080004;
@@ -42,17 +46,22 @@ public final class R {
         public static final int button9=0x7f08000b;
         public static final int close=0x7f080001;
         public static final int grid_view=0x7f080002;
-        public static final int lessonHeader=0x7f08000e;
-        public static final int lessonText=0x7f08000f;
-        public static final int myimage1=0x7f080012;
-        public static final int myimage2=0x7f080014;
-        public static final int myimage3=0x7f080016;
-        public static final int myimage4=0x7f080018;
-        public static final int popup=0x7f08000d;
-        public static final int start=0x7f080010;
+        public static final int imageView1=0x7f08000e;
+        public static final int imageView2=0x7f080010;
+        public static final int imageView3=0x7f08000f;
+        public static final int lessonHeader=0x7f080013;
+        public static final int lessonText=0x7f080014;
+        public static final int myimage1=0x7f080017;
+        public static final int myimage2=0x7f080019;
+        public static final int myimage3=0x7f08001b;
+        public static final int myimage4=0x7f08001d;
+        public static final int next=0x7f080011;
+        public static final int popup=0x7f080012;
+        public static final int start=0x7f080015;
         public static final int textView1=0x7f080000;
-        public static final int topleft=0x7f080011;
-        public static final int topright=0x7f080013;
+        public static final int textView2=0x7f08000d;
+        public static final int topleft=0x7f080016;
+        public static final int topright=0x7f080018;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/neonbats/mathlove/LessonOne.java
+++ src/com/neonbats/mathlove/LessonOne.java
@@ -1,13 +1,16 @@
 package com.neonbats.mathlove;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class LessonOne extends Activity {
+	
 	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.lessonone);
 	}
-
+	
 }
