aabe34d137d1d72259b7e9358f2bb99e7668f109
Daniel Laurence <dannyjlaurence@hotmail.com>
2013-12-15 12:44:10 -0500

    Path Screen is done

--- src/com/neonbats/mathlove/LessonMap.java
+++ src/com/neonbats/mathlove/LessonMap.java
@@ -2,16 +2,13 @@ package com.neonbats.mathlove;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TableLayout;
 
 public class LessonMap extends Activity {
 
 	public void onCreate(Bundle savedInstanceState) {
 		 super.onCreate(savedInstanceState);
 		 setContentView(R.layout.lessonmap);
-		 
-		 TableLayout ll = (TableLayout)findViewById(R.id.grid_view);
-		 ll.setBackgroundResource(R.drawable.path);
+		
 		 
 	}
 	
