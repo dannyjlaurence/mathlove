400db45e97044a41c10a0382aacdd756f1e20094
Daniel Laurence <dannyjlaurence@hotmail.com>
2013-12-15 14:10:34 -0500

    Lesson pop-ups

--- src/com/neonbats/mathlove/LessonMap.java
+++ src/com/neonbats/mathlove/LessonMap.java
@@ -1,15 +1,112 @@
 package com.neonbats.mathlove;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class LessonMap extends Activity {
 
 	public void onCreate(Bundle savedInstanceState) {
 		 super.onCreate(savedInstanceState);
 		 setContentView(R.layout.lessonmap);
-		
-		 
 	}
+	
+	
+	// Implement the OnClickListener callback
+    public void onClick(View v) {
+      // do something when the button is clicked
+    	Log.i("test","totally worked");
+    	Dialog dialog;
+    	
+    	switch(v.getId()){
+    	case R.id.button1:
+    		
+    		dialog = new Dialog(LessonMap.this);
+        	dialog.setContentView(R.layout.lessonpopup);
+        	dialog.setTitle("Lesson 1");
+        	dialog.show();
+    		
+    		break;
+    	case R.id.button2:
+    		
+    		dialog = new Dialog(LessonMap.this);
+        	dialog.setContentView(R.layout.lessonpopup);
+        	dialog.setTitle("Lesson 2");
+        	dialog.show();
+    		
+    		break;
+    	case R.id.button3:
+    		
+    		dialog = new Dialog(LessonMap.this);
+        	dialog.setContentView(R.layout.lessonpopup);
+        	dialog.setTitle("Lesson 3");
+        	dialog.show();
+    		
+    		break;
+    	case R.id.button4:
+    		
+    		dialog = new Dialog(LessonMap.this);
+        	dialog.setContentView(R.layout.lessonpopup);
+        	dialog.setTitle("Lesson 4");
+        	dialog.show();
+    		
+    		break;
+    	case R.id.button5:
+    		
+    		dialog = new Dialog(LessonMap.this);
+        	dialog.setContentView(R.layout.lessonpopup);
+        	dialog.setTitle("Lesson 5");
+        	dialog.show();
+    		
+    		break;
+    	case R.id.button6:
+    		
+    		dialog = new Dialog(LessonMap.this);
+        	dialog.setContentView(R.layout.lessonpopup);
+        	dialog.setTitle("Lesson 6");
+        	dialog.show();
+    		
+    		break;
+    	case R.id.button7:
+    		
+    		dialog = new Dialog(LessonMap.this);
+        	dialog.setContentView(R.layout.lessonpopup);
+        	dialog.setTitle("Lesson 7");
+        	dialog.show();
+    		
+    		break;
+    	case R.id.button8:
+    		
+    		dialog = new Dialog(LessonMap.this);
+        	dialog.setContentView(R.layout.lessonpopup);
+        	dialog.setTitle("Lesson 8");
+        	dialog.show();
+    		
+    		break;
+    	case R.id.button9:
+    		
+    		dialog = new Dialog(LessonMap.this);
+        	dialog.setContentView(R.layout.lessonpopup);
+        	dialog.setTitle("Lesson 9");
+        	dialog.show();
+    		
+    		break;
+    	case R.id.button10:
+    		
+    		dialog = new Dialog(LessonMap.this);
+        	dialog.setContentView(R.layout.lessonpopup);
+        	dialog.setTitle("Lesson 10");
+        	dialog.show();
+    		
+    		break;
+    		
+    		
+    	
+    	}
+    	    	
+    }
 	
 }
